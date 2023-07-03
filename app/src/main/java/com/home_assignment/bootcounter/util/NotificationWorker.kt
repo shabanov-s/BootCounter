5711cb6599c52829eeff06d8968d4cc599835b40
Svyatosolav Shabanov <shabanov_s@protonmail.com>
2023-07-03 19:02:08 +0300

    impl

--- app/src/main/java/com/home_assignment/bootcounter/util/NotificationWorker.kt
+++ app/src/main/java/com/home_assignment/bootcounter/util/NotificationWorker.kt
@@ -5,6 +5,10 @@ import android.content.Context
 import androidx.core.app.NotificationCompat
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import com.home_assignment.bootcounter.data.database.BootEventDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class NotificationWorker(private val context: Context, workerParams: WorkerParameters) :
     Worker(context, workerParams) {
@@ -13,14 +17,28 @@ class NotificationWorker(private val context: Context, workerParams: WorkerParam
         val notificationManager =
             applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
-        val notificationBuilder = NotificationCompat.Builder(context, CHANNEL_ID)
-            .setContentTitle("Boot Counter")
-            .setContentText("Recurring task executed")
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        val dao = BootEventDatabase.getInstance(applicationContext).bootEventDao()
 
-        val notification = notificationBuilder.build()
-        notificationManager.notify(NOTIFICATION_ID, notification)
+        CoroutineScope(Dispatchers.IO).launch {
+            val events = dao.getBootEvent()
+            val result = if (events.isEmpty()) {
+                "No boots detected"
+            } else {
+                // todo: calculate diff between two boots and handle the single event
+                events.mapIndexed { index, event ->
+                    "$index - ${event.timestamp}"
+                }.joinToString { "\n" }
+            }
 
+            val notificationBuilder = NotificationCompat.Builder(context, CHANNEL_ID)
+                .setContentTitle("Boot Counter")
+                .setContentText(result)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+            val notification = notificationBuilder.build()
+            notificationManager.notify(NOTIFICATION_ID, notification)
+
+        }
         return Result.success()
     }
 
